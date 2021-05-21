3cc4d712a1977de56967d813ff2eb0ee71248128
ivis-kuwata <76928519+ivis-kuwata@users.noreply.github.com>
2021-05-21 15:24:24 +0900

    fix: git_config

--- 004_git_config.sh
+++ 004_git_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config --global user.name 'ivis-kuwata'
-git config --global user.email 'jumpei.kuwata@ivis.co.jp'
+git config --global user.name 'xxx'
+git config --global user.email 'xxx@xxx.com'
 git config --global core.editor 'code --wait'
 git config --global merge.tool 'code --wait "$MERGED"'
 git config --global push.default simple
